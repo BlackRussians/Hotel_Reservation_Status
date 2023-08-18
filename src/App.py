6f96eb87f2f4ad0bec91e5f452a866acd47ecbfc
Soobin Choi <jkz@naver.com>
2023-08-18 13:46:24 +0900

    fix(chrome): update selenium library
    update selenium library

--- src/App.py
+++ src/App.py
@@ -2,11 +2,11 @@ import time
 import colorama
 from common.Interface import Interface
 from util.color import Color
+from config import __VERSION__
 
 
 def main():
-    __version__ = "1.0.1.4"
-    print(f"{Color.YELLOW}Ver {__version__}{Color.END}")
+    print(f"{Color.YELLOW}Ver {__VERSION__}{Color.END}")
     start_tm = time.time()  # 시간 측정 시작
     interface.main_menu()
     end_tm = time.time() - start_tm  # 시간 측정 종료
